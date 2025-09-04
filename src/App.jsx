da3dbc34444f3800d1882bbd05f58c38ae9e1d66
Carlos Ibanez <carlos.ibanez@live.se>
2025-09-04 11:10:08 +0200

    Lagt till en React project och lagt till Lennarts komponenter så de syns vid npm run dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css';
+import Board from './components/Board/Board';
+import Cell from './components/Cell/Cell';
+import Footer from './components/Footer/Footer';
+import Game from './components/Game/Game';
+import React from 'react';  
+import Header from './components/Header/Header';
 
 function App() {
-  return hej;
+  return (
+    <>
+      <Board />
+      <Cell />
+      <Footer />
+      <Game />
+      <Header />   
+     </>
+  );
 }
 
 export default App;
